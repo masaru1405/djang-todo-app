6d5b0750dbd1e951ba8d45a7dd538412fdb5f009
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2022-09-21 15:44:50 -0300

    todoitem view criado

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import ToDoList, ToDoItem
+
+class ToDoListListView(ListView):
+   model = ToDoList
+   context_object_name = 'todos'
+   template_name = 'todo_app/index.html'
+
+class ToDoItemListView(ListView):
+   model = ToDoItem
+   context_object_name = 'todoitem'
+   template_name = "todo_app/todo_list.html"
+
+   def get_queryset(self):
+      return ToDoItem.objects.filter(todo_list_id=self.kwargs["list_id"])
+   
+   def get_context_data(self):
+      context = super().get_context_data()
+      context['todo_list'] = ToDoList.objects.get(id=self.kwargs["list_id"])
+      return context
